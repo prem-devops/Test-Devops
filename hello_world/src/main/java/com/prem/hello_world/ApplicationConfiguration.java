fd714d036140ac8aa1fe7baa8f306e431f69e858
prem <prem@192.168.0.105>
2019-10-07 19:58:39 +0530

    My first spring boot  app with devops and aws co-ordination.

---
+++ hello_world/src/main/java/com/prem/hello_world/ApplicationConfiguration.java
@@ -0,0 +1,17 @@
+package com.prem.hello_world;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ApplicationConfiguration {
+
+	@RequestMapping("hello")
+	public String hello()
+	{
+		return "Hello from Prem !!!";
+	}
+	@RequestMapping("welcome")
+	public String welcome()
+	{
+		return "Welcome from Prem !!!";
+	}
+}
